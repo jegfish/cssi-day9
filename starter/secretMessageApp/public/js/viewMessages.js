899e950f35454c72b7e68580e33641f326cfda51
Jeffrey Fisher <jegfish@fastmail.com>
2021-07-22 17:53:13 +0000

    CSSI Day 9 Code-Along

--- starter/secretMessageApp/public/js/viewMessages.js
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -1,26 +1,51 @@
-const getMessages = () => {
+// const getMessages = () => {
+//     const messagesRef = firebase.database().ref();
+//     messagesRef.on("value", (snapshot) => {
+//         const data = snapshot.val();
+//         console.log(data);
+//         for (let key in data) {
+//             const message = data[key];
+//             console.log(message);
+//         }
+//     });
+// }
+
+// const findMessage = (myPass) => {
+//     const messagesRef = firebase.database().ref();
+//     messagesRef.on("value", (snapshot) => {
+//         const data = snapshot.val();
+//         // console.log(data);
+//         for (let key in data) {
+//             const message = data[key];
+//             console.log(message);
+//             if (myPass === message.passcode) {
+//                 renderMessage(message);
+//             }
+//         }
+//     });
+// }
+
+document.querySelector("#viewMsg").addEventListener("click", (event) => {
+    const userPasscodeGuess = document.querySelector("#passcode").value;
+    console.log("user guess:", userPasscodeGuess);
+    // findMessage(userPasscodeGuess);
+
     const messagesRef = firebase.database().ref();
-    messagesRef.on('value', (snapshot) => {
-    const data = snapshot.val();
-    findMessage(data);
-    });
-}
-
-const findMessage = (messages) => {
-    const passcodeAttempt = document.querySelector('#passcode').value;
-    for(message in messages) {
-        const messageData = messages[message];
-        if(messageData.passcode === passcodeAttempt) {
-            renderMessageAsHtml(messageData.message)
+    messagesRef.on("value", (snapshot) => {
+        const data = snapshot.val();
+        for (let key in data) {
+            // console.log(data[key]);
+            if (data[key].passcode == userPasscodeGuess) { // type coercion ==
+                displayMessage(data[key]);
+            }
         }
-    }
-}
+    });
+});
 
-const renderMessageAsHtml = (message) => {
-    // Hide Input Form
-    const passcodeInput = document.querySelector('#passcodeInput');
-    passcodeInput.style.display = 'none';
-    // Render messageas HTML
-    const messageDiv = document.querySelector('#message');
-    messageDiv.innerHTML = message;   
+function
+displayMessage(msg)
+{
+    // document.querySelector("#")
+    console.log(msg);
+    document.querySelector("#message").innerHTML = msg.message;
 }
